c19b5fd975c960486f1b1f9cb75e32227a3a7898
shatam-25 <shatam-25@shatam-25>
2023-04-04 19:34:05 +0530

    test commit

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is my first git project.");
 		System.out.println("trying to commit this to github.");
+		System.out.println("This is another commit");
 	}
 
 }
